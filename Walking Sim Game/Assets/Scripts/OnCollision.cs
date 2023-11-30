418fa71befc6f7bc08deb293a17da1e53c4f8dec
Daniel DiSessa <ddisessa@student.fitchburgstate.edu>
2023-11-30 10:59:05 -0500

    collision and ui

---
+++ Walking Sim Game/Assets/Scripts/OnCollision.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        KeepScore.Paper += 1;
+    }
+
+}
