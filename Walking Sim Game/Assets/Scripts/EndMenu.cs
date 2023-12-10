f3e3d8febb4cb730e5e7d6bb34ec5a235489facb
Daniel DiSessa <ddisessa@student.fitchburgstate.edu>
2023-12-10 12:25:28 -0500

    Sounds/Maze Update

--- Walking Sim Game/Assets/Scripts/EndMenu.cs
+++ Walking Sim Game/Assets/Scripts/EndMenu.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class EndMenu : MonoBehaviour
 {
+    private AudioSource playerAudio;
+    public AudioClip EndSound;
+    private float Endaudio = 1.0f;
+
+    void Start()
+    {
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.tag == "Player")
         {
+            playerAudio.PlayOneShot(EndSound, Endaudio);
             SceneManager.LoadScene("EndGameMenu");
-
+            
         }
     }
 }
--- Walking Sim Game/Assets/Scripts/KeepScore.cs
+++ Walking Sim Game/Assets/Scripts/KeepScore.cs
@@ -8,20 +8,31 @@ public class KeepScore : MonoBehaviour
     public GameObject wall1;
     public GameObject wall2;
     public GameObject text;
+    private AudioSource playerAudio;
+    public AudioClip ExitSound;
+    private float Exitaudio = 1.0f;
+    public AudioClip CrowSound;
+    private float Crowaudio = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Paper >= 1)
+        if(Paper >= 5)
         {
             wall1.SetActive(false);
             wall2.SetActive(false);
             text.SetActive(true);
+            playerAudio.PlayOneShot(ExitSound, Exitaudio);
+        }
+
+        if (Paper >= 3)
+        {
+            playerAudio.PlayOneShot(CrowSound, Crowaudio);
         }
     }
 
